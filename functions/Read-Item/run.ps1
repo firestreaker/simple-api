673befe7bd35b71bd5670ed1b8bd2ff608edb557
firestreaker <brian_coolkid@yahoo.com>
2021-02-07 21:46:21 +1100

    Initial commit

---
+++ functions/Read-Item/run.ps1
@@ -0,0 +1,23 @@
+using namespace System.Net
+
+param($Request, $TriggerMetadata)
+
+# Write to the Azure Functions log stream.
+Write-Host "PowerShell HTTP trigger function processed a request."
+
+try{
+    #Get item here
+    $body = @{}
+    $status = [HttpStatusCode]::OK
+}
+catch{
+    $status = $status ?? $_.Exception.Response.StatusCode ?? [HttpStatusCode]::BadRequest
+    $body = @{ Error = $_.Exception.Message }
+    if ($_.Exception.Response.RequestMessage.RequestUri) { $body.Add("RequestUri", $_.Exception.Response.RequestMessage.RequestUri) }
+}
+
+# Associate values to output bindings by calling 'Push-OutputBinding'.
+Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+    StatusCode = $status
+    Body = $body
+})
